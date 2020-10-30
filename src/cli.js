425548766e057c3d0b5e7b76996e225f6caed4bb
cryptobfund <cryptobfund@gmail.com>
2020-10-30 16:19:15 +0300

    step 3

---
+++ src/cli.js
@@ -0,0 +1,7 @@
+import promptly from 'promptly';
+
+export default async () => {
+    console.log('May I have your name?');
+    const name = await promptly.prompt('Your answer: ');
+    console.log(`Hello, ${name}!`);
+};
